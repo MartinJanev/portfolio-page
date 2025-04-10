1312e6860f1327973190d77a687e05b232823ac5
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2025-04-10 17:43:18 +0200

    fix loading

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -18,7 +18,7 @@ export const LoadingScreen = ({ onComplete }) => {
           onComplete();
         }, 1000);
       }
-    }, 100);
+    }, 75);
 
     return () => clearInterval(interval);
   }, [onComplete]);
@@ -26,10 +26,11 @@ export const LoadingScreen = ({ onComplete }) => {
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
       <div className="nb-4 text-4xl font-mono font-bold">
-        {text} <span className="animate-blink ml-1"> | </span>
+        {text}
+        <span className="animate-blink ml-1">| </span>
       </div>
-
-      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+      <br></br>
+      <div className="w-[300px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-green-500 shadow-[0_0_15_$3b82f6] animate-loading-bar">
           {" "}
         </div>
